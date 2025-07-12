3e67d5ea90cee49c6b77463bc747b54bfa448a05
kiharu12 <yugo200412@gmail.com>
2025-07-12 15:31:42 +0900

    コンパイルエラーを解消

--- Assets/Scenes/Sandbox/Is/broken floor_Is.cs
+++ Assets/Scenes/Sandbox/Is/broken floor_Is.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public class brokenfloor_Is : MonoBehaviour
 {
-    public enum Type { go, notgo}
+    public enum Type { go, notgo }
+    
+    public Type type = Type.go; // 床の状態を設定する（進めるかどうか）
 }
