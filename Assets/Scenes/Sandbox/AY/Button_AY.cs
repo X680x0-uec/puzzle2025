4feedb7df31771ccaa867b204938368fb230ae7a
Usekey <a2512003@gl.cc.uec.ac.jp>
2025-08-24 20:49:02 +0900

    開発中に用いたテスト用のコマンドを削除

--- Assets/Scenes/Sandbox/AY/Button_AY.cs
+++ Assets/Scenes/Sandbox/AY/Button_AY.cs
@@ -5,7 +5,7 @@ public class Button_AY : MonoBehaviour
     public BrokenWall_AY recever;
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("aaaa");
+
         if (other.CompareTag("Player"))
         {
             if (recever != null)
