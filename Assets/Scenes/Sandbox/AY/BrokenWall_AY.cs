fbde545f16ef62ba384ec58b4934fc41e1da763c
Usekey <a2512003@gl.cc.uec.ac.jp>
2025-09-28 20:19:30 +0900

    Reapply "ボタンを一度踏んだら消えるボタン(オレンジ)と踏んでも消えないボタン(黄緑)の二つに分けて実装"
    This reverts commit 81f61ac969bff20f135f71d8de2bda214a1ac28d.

--- Assets/Scenes/Sandbox/AY/BrokenWall_AY.cs
+++ Assets/Scenes/Sandbox/AY/BrokenWall_AY.cs
@@ -2,27 +2,22 @@ using UnityEngine;
 
 public class BrokenWall_AY : MonoBehaviour
 {
-    public GameObject BrokenWallAfter;
+
     public GameObject BrokenWallBefore; // BrokenWallを追加
-    void Start()
-    {
-        if (BrokenWallAfter != null) // ②
-        {
 
-
-            BrokenWallAfter.SetActive(false);
-
-        }
-    }
 
     public void BreakeWall()
     {
-        BrokenWallBefore.SetActive(false);
-        if (BrokenWallAfter != null) // ⑥
-        {
-            BrokenWallAfter.SetActive(true); // ⑦
-        }
 
+        if (BrokenWallBefore.activeSelf) // Check if the "before" wall is active
+        {
+            BrokenWallBefore.SetActive(false); // If it is, deactivate it
+        }
+        else
+        {
+            BrokenWallBefore.SetActive(true); // If it's not, activate it
+        }
     }
 
 }
+
